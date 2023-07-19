d10eb8987b80eb87f6c772a91cbaf4173b58dc53
kaitiaholcomb <134977932+kaitiaholcomb@users.noreply.github.com>
2023-07-19 17:59:01 -0400

    Update GUI.py
    Made a few changes to the responses as well as updated the program's name to 'Magically Magic 8 Ball'

--- GUI.py
+++ GUI.py
@@ -7,7 +7,7 @@ customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.Ctk()
 
-root.title('Tkinter.com - Magic 8-Ball')
+root.title('Tkinter.com - Magically Magic 8 Ball')
 root.iconbitmap()
 root.geometry("500X500")
 
@@ -16,11 +16,11 @@ def shake():
         "It is certain.": "green",
         "It is decidedly so.": "green",
         "Without a doubt.": "green",
-        "Yes definitely.": "green",
+        "Yes, definitely.": "green",
         "You may rely on it.": "green",
         "As I see it, yes.": "green",
         "Most likely.": "green",
-        "Outlook good.": "green",
+        "Outlook is good.": "green",
         "Yes.": "green",
         "Signs point to yes.": "green",
         "Reply hazy, try again.": "yellow",
@@ -31,7 +31,7 @@ def shake():
         "Do not count on it.": "red",
         "My reply is no.": "red",
         "My sources say no.": "red",
-        "Outlook not so good.": "red",
+        "Outlook is not so good.": "red",
         "Very doubtful.": "red"]
     responses_list = list(responses.items())
 
@@ -47,5 +47,5 @@ ball_img.pack(pady=35)
 results = Label(root, text="", font=("Helvetica", 28), bg= "#1a1a1a")
 results.pack(pady=20)
 
-ball_button = customtkinter.CTkButton(master=root, text= "Shake the Magic 8 ball!", width= 190, height=40, compound="top", command=shake)
+ball_button = customtkinter.CTkButton(master=root, text= "Shake the Magically Magic 8 Ball!", width= 190, height=40, compound="top", command=shake)
 ball_button.pack(pady=30)
